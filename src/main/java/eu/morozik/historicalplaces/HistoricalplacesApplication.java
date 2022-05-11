48fde66200d2f02c7528029f1a865ad4f70cb080
Yan Moro <yan.morozik24@mail.ru>
2022-05-12 00:56:40 +0300

    Initial commit

---
+++ src/main/java/eu/morozik/historicalplaces/HistoricalplacesApplication.java
@@ -0,0 +1,13 @@
+package eu.morozik.historicalplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalplacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoricalplacesApplication.class, args);
+    }
+
+}
